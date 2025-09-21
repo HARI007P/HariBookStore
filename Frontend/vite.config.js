0d5397411c90dbee98317ed374b64c171ad72e05
hari narayana <119913719+HARI007P@users.noreply.github.com>
2025-09-21 13:14:38 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    port: 3000
+  base: './', // ensures assets load correctly in static hosting
+  build: {
+    outDir: 'dist', // default Vite build folder
   }
 })
